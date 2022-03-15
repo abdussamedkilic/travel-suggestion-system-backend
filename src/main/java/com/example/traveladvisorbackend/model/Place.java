73147c0efba8b47e79668ecef5950ef0fec5a31f
İsmail Coşkun <ismail.coskun@obss.com.tr>
2022-03-15 17:52:31 +0300

    model , dto and enum classes added

---
+++ src/main/java/com/example/traveladvisorbackend/model/Place.java
@@ -0,0 +1,40 @@
+package com.example.traveladvisorbackend.model;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+@Slf4j
+@Data
+@Document("Place")
+public class Place {
+    @Id
+    private String id;
+
+    @Field(name = "name")
+    private String name;
+
+    @Field(name = "location")
+    private String location;
+
+    @Field(name = "type")
+    private PlaceType type;
+
+    @Field(name = "image")
+    private String image;
+
+    @Field(name = "detail")
+    private String detail;
+
+    @Field(name = "rating")
+    private double rating;
+
+    @Field(name = "comments")
+    private List<String> comments;
+
+
+}
